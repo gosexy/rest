6e80a4988ee1e06d8d50ec2e6784a2f2ca791030
José Carlos Nieto <jose.carlos@menteslibres.net>
2013-10-12 08:56:35 -0500

    Adding an example that uses rest.Response.

---
+++ _examples/rest-response/main.go
@@ -0,0 +1,46 @@
+// This is an example for the gosexy/rest package.
+
+package main
+
+import (
+	// Import the gosexy/rest package.
+	"log"
+	"menteslibres.net/gosexy/rest"
+	"net/url"
+)
+
+func main() {
+
+	var err error
+
+	// You may want to see the full client debug.
+	// rest.Debug = true
+
+	// Destination variable.
+	buf := rest.Response{}
+
+	// A nice gopher image.
+	requestURL := "https://api.twitter.com/v1/foo.json"
+
+	// We don't need any GET vars.
+	requestVariables := url.Values{}
+
+	// Let's pass buf's address ad first argument.
+	err = rest.Get(&buf, requestURL, requestVariables)
+
+	// Was there any error?
+	if err == nil {
+
+		// Printing response dump.
+		log.Printf("Got response!")
+		log.Printf("Response code: %d", buf.StatusCode)
+		log.Printf("Response proto: %s", buf.Proto)
+		log.Printf("Response length: %d", buf.ContentLength)
+		log.Printf("Response header: %v", buf.Header)
+		log.Printf("Response body: %s", string(buf.Body))
+	} else {
+		// Yes, we had an error.
+		log.Printf("Error getting URL: %s", err.Error())
+	}
+
+}
