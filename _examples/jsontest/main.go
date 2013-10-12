01b24a7920ea83e76d5cbe0ac1e17905ee4b9026
José Carlos Nieto <jose.carlos@menteslibres.net>
2013-10-12 07:19:32 -0500

    An example that prints your IP via ip.jsontest.com.

---
+++ _examples/jsontest/main.go
@@ -0,0 +1,44 @@
+// This is an example for the gosexy/rest package.
+
+package main
+
+import (
+	// Import the gosexy/rest package.
+	"log"
+	"menteslibres.net/gosexy/rest"
+	"net/url"
+)
+
+func main() {
+
+	// Destination variable.
+	buf := map[string]interface{}{}
+
+	// This service returns a JSON string containing your IP, like:
+	// {"ip": "173.194.64.141"}
+	requestURL := "http://ip.jsontest.com/"
+
+	// We don't need any GET vars.
+	requestVariables := url.Values{}
+
+	// Let's pass buf's address ad first argument.
+	err := rest.Get(&buf, requestURL, requestVariables)
+
+	// Was there any error?
+	if err == nil {
+
+		// Printing response dump.
+		log.Printf("Got response: buf = %v\n", buf)
+
+		// Expecting a map with a single "ip" key.
+		if ip, ok := buf["ip"].(string); ok {
+			// What is my IP?
+			log.Printf("According to ip.jsontest.com, your IP is %s\n", ip)
+		}
+
+	} else {
+		// Yes, we had an error.
+		log.Printf(err.Error())
+	}
+
+}
